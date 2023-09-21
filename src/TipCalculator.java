7eb34dc4273eb864749e25ddf636dbbe054fabc1
BT_1N3_07 <sample@bths.edu>
2023-09-21 10:31:30 -0400

    find total bill and prints it out

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -3,10 +3,22 @@ public class TipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
                 System.out.println("Welcome to the tip calculator!");
-                System.out.println("How many people are in your group: ");
+                System.out.print("How many people are in your group: ");
                 int groupNumber = scan.nextInt();
                 System.out.println("What's the tip percentage? (0-100): ");
                 double tipPercentage = scan.nextDouble();
+                System.out.println("Enter a cost in dollars and cents, e.g. 3.65 (-1 to end): ");
+                double input = scan.nextDouble();
+
+                double totalBill = 0;
+                while (input != -1) {
+                    System.out.println("Enter a cost in dollars and cents, e.g. 3.65 (-1 to end): ");
+                    input = scan.nextDouble();
+                    totalBill += input;
+                }
+                System.out.println();
+                System.out.println("Total bill before tip: " + totalBill);
+
 
     }
 }
